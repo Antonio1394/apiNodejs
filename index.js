65986ba01a432ff6e2ad650953103d439d367771
Antonio1394 <antoniorivm1394@gmail.com>
2016-10-20 23:09:36 -0600

    [add] conection mongoDb

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express=require('express');
 var mongoose=require('mongoose');
 var app=express();
 
+mongoose.connect('mongodb://localhost/seriestv',function(err, res){
+	if(err) console.log('Error> conectando a la base de datos: '+err);
+	else console.log('Conexion Exitosa a la base de datos');
+});
+
 app.get(function(){
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
