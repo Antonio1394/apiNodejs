cfb2df4886b778ddab41204a1ab673c913337cd4
Antonio1394 <antoniorivm1394@gmail.com>
2016-10-26 23:42:41 -0600

    [add] method post :bear:

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ module.exports=function(app){
 			if(!err) res.send(serietv);
 			else console.log('ERROR'+ err);
 		});
-	}
+	};
 
 	//GET
 	findByID=function(req,res){
@@ -15,5 +15,24 @@ module.exports=function(app){
 			if(!err) res.send(serietv);
 			else console.log('ERROR'+ err);
 		});
+	};
+
+	//POST
+	addSerieTV=function(req,res){
+		console.log('POST');
+		console.log(req.body);
+		var serietv =new SerieTV({
+			titulo:req.body.titulo,
+			temporadas:req.body.temporadas,
+			pais:req.body.pais,
+			genero.req.body.genero,
+		});
+
+		SerieTV.save(function(err){
+			if(!err) console.log('SerieTV Guardad');
+			else console.log('ERROR'+ err);
+		});
+
+		res.send(serietv);
 	}
 }	
