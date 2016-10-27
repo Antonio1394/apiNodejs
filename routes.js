4a95b80cfc519ddf11490c67c3f5747554b0e064
Antonio1394 <antoniorivm1394@gmail.com>
2016-10-26 23:34:05 -0600

    [add] routes get :bomb:

--- routes.js
+++ routes.js
@@ -4,7 +4,16 @@ module.exports=function(app){
 	//GET
 	findAllSeriesTV=function(req, res){
 		SerieTV.find(function(err,serietv){
+			if(!err) res.send(serietv);
+			else console.log('ERROR'+ err);
+		});
+	}
 
+	//GET
+	findByID=function(req,res){
+		SerieTV.findByID(req.param.id,function(err,serietv){
+			if(!err) res.send(serietv);
+			else console.log('ERROR'+ err);
 		});
 	}
 }	
